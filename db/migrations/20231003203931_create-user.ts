46e6b2ef2cc4f68081b0af6aa81443f300d6df90
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-10-03 18:51:40 -0300

    chore: retirado gitignore /db para subir as migrations, app.db continua no gitignore

---
+++ db/migrations/20231003203931_create-user.ts
@@ -0,0 +1,14 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("user", (table) => {
+    table.uuid("id").primary();
+    table.text("name").notNullable();
+    table.text("lastname").notNullable();
+    table.text("email").notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("user");
+}
