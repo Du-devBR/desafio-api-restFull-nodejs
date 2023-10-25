55ba9b00e6c3a6afe6a0e9dc48c620d5532e0fda
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-10-25 18:22:37 -0300

    tests: Adicionado token para login

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -24,31 +24,58 @@ describe("User routes", () => {
     execSync("npm run knex migrate:latest");
   });
 
-  describe("User regitration", () => {
-    test("User can regiter", async () => {
-      const response = await request(app.server).post("/user").send({
+  describe("User authentication", () => {
+    test("User can register", async () => {
+      const response = await request(app.server).post("/user/register").send({
         name: "New user",
         lastname: "Lastname User",
         email: "newuser@teste.com",
+        password: "12345678",
       });
       expect(response.statusCode).toEqual(201);
+    });
+    test("User can login", async () => {
+      const response = await request(app.server).post("/user/register").send({
+        name: "New user",
+        lastname: "Lastname User",
+        email: "newuser@teste.com",
+        password: "12345678",
+      });
+      const userLoginResponse = await request(app.server)
+        .post("/user/login")
+        .send({
+          email: "newuser@teste.com",
+          password: "12345678",
+        });
+      expect(userLoginResponse.statusCode).toEqual(201);
     });
   });
 
   describe("User can manege meals", () => {
     let idUser: string;
     let idMeal: string;
+    let authToken: string;
 
     beforeEach(async () => {
-      const createUserResponse = await request(app.server).post("/user").send({
-        name: "New user",
-        lastname: "Lastname User",
-        email: "newuser@teste.com",
-      });
+      const createUserResponse = await request(app.server)
+        .post("/user/register")
+        .send({
+          name: "New user",
+          lastname: "Lastname User",
+          email: "newuser@teste.com",
+          password: "12345678",
+        });
       idUser = createUserResponse.body.id;
+
+      const response = await request(app.server).post("/user/login").send({
+        email: "newuser@teste.com",
+        password: "12345678",
+      });
+      authToken = response.body.token;
 
       const createMealresponse = await request(app.server)
         .post(`/user/${idUser}/meal`)
+        .set("Authorization", `Bearer ${authToken}`)
         .send({
           name: "Refeição teste",
           description: "Refeição feita para teste",
@@ -62,6 +89,7 @@ describe("User routes", () => {
     test("User can register a meal", async () => {
       const response = await request(app.server)
         .post(`/user/${idUser}/meal`)
+        .set("Authorization", `Bearer ${authToken}`)
         .send({
           name: "Refeição teste",
           description: "Refeição feita para teste",
@@ -72,17 +100,22 @@ describe("User routes", () => {
       expect(response.statusCode).toEqual(201);
     });
     test("User can view yours meals", async () => {
-      const response = await request(app.server).get(`/user/${idUser}/meal`);
+      const response = await request(app.server)
+        .get(`/user/${idUser}/meal`)
+        .set("Authorization", `Bearer ${authToken}`);
       expect(response.statusCode).toEqual(200);
     });
     test("User can view details of a meal by id", async () => {
-      const response = await request(app.server).get(`/user/${idUser}/meal`);
+      const response = await request(app.server)
+        .get(`/user/${idUser}/meal`)
+        .set("Authorization", `Bearer ${authToken}`);
 
       expect(response.statusCode).toEqual(200);
     });
     test("User can edit a meal by id", async () => {
       const response = await request(app.server)
         .put(`/user/${idUser}/meal/${idMeal}`)
+        .set("Authorization", `Bearer ${authToken}`)
         .send({
           name: "Modificado refeição",
           description: "Modificado Refeição feita para teste",
@@ -94,32 +127,50 @@ describe("User routes", () => {
     });
 
     test("User can delete a meal by id", async () => {
-      const response = await request(app.server).delete(
-        `/user/${idUser}/meal/${idMeal}`,
-      );
+      const response = await request(app.server)
+        .delete(`/user/${idUser}/meal/${idMeal}`)
+        .set("Authorization", `Bearer ${authToken}`);
       expect(response.statusCode).toEqual(200);
     });
   });
 
   describe("User metrics", () => {
     test("User can view your metrics", async () => {
-      const createUserResponse = await request(app.server).post("/user").send({
-        name: "New user",
-        lastname: "Lastname User",
-        email: "newuser@teste.com",
-      });
+      const createUserResponse = await request(app.server)
+        .post("/user/register")
+        .send({
+          name: "New user",
+          lastname: "Lastname User",
+          email: "newuser@teste.com",
+          password: "12345678",
+        });
 
       const idUser = createUserResponse.body.id;
 
-      await request(app.server).post(`/user/${idUser}/meal`).send({
-        name: "Refeição teste",
-        description: "Refeição feita para teste",
-        isDiet: true,
-        createdAt: "2023-11-28T21:00:00.000-03",
-        userId: idUser,
-      });
+      const userLoginResponse = await request(app.server)
+        .post("/user/login")
+        .send({
+          email: "newuser@teste.com",
+          password: "12345678",
+        });
 
-      const response = await request(app.server).get(`/user/${idUser}/metrics`);
+      const authToken = userLoginResponse.body.token;
+      console.log(authToken);
+
+      await request(app.server)
+        .post(`/user/${idUser}/meal`)
+        .set("Authorization", `Bearer ${authToken}`)
+        .send({
+          name: "Refeição teste",
+          description: "Refeição feita para teste",
+          isDiet: true,
+          createdAt: "2023-11-28T21:00:00.000-03",
+          userId: idUser,
+        });
+
+      const response = await request(app.server)
+        .get(`/user/${idUser}/metrics`)
+        .set("Authorization", `Bearer ${authToken}`);
 
       expect(response.statusCode).toEqual(200);
     });
